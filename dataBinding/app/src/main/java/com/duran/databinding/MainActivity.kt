7f4e2ffd6811af716afb145795ac12d21793b683
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-01 22:30:16 +0900

    dataBinding을 사용해여 text 변경해보기 / dataBinding의 xml에서 특징

--- dataBinding/app/src/main/java/com/duran/databinding/MainActivity.kt
+++ dataBinding/app/src/main/java/com/duran/databinding/MainActivity.kt
@@ -2,10 +2,26 @@ package com.duran.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.duran.databinding.databinding.ActivityMainBinding
+
+/*
+DataBinding
+
+ViewBinding 과 무엇이 다른가?
+이름처럼 데이터를 연결해주는 역할을 할 수 있다.(데이터와 같이 결합해서 사용할 수 있음)
+DataBinding + LiveData
+*/
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dataBindingEx.text = "DataBinding을 사용해서 바뀐 텍스트"
+
     }
 }
