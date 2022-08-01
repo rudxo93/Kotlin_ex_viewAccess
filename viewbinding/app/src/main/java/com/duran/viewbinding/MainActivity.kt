9fbf56bf832b939c7e2900c37bb1a50a7275d9cb
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-01 17:31:48 +0900

    viewBinding을 사용해서 간단하게 text변경

--- viewbinding/app/src/main/java/com/duran/viewbinding/MainActivity.kt
+++ viewbinding/app/src/main/java/com/duran/viewbinding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.duran.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.duran.viewbinding.databinding.ActivityMainBinding
+
+/*
+ViewBinding
+
+Activity와 Fragment 사용하는 방법을 숙지
+*/
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.testText.text = "이것은 변경된 텍스트입니다."
     }
 }
