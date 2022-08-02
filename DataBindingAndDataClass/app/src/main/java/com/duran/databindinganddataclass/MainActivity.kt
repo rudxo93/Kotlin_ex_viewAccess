d2bb2391d23f1b04ad51dce3f82d63476de7617c
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-02 10:59:05 +0900

    기존에 방법이 아닌 데이터 결합 후 텍스트 변경

--- DataBindingAndDataClass/app/src/main/java/com/duran/databindinganddataclass/MainActivity.kt
+++ DataBindingAndDataClass/app/src/main/java/com/duran/databindinganddataclass/MainActivity.kt
@@ -2,6 +2,8 @@ package com.duran.databindinganddataclass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.duran.databindinganddataclass.databinding.ActivityMainBinding
 
@@ -16,12 +18,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    var testCount = 30
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
 
         // 기존의 방법
-        binding.test.text = "데이터 바인딩을 사용하여 바뀐 텍스트"
+        // binding.test.text = "데이터 바인딩을 사용하여 바뀐 텍스트"
+
+        // 데이터 결합
+        val person = Person("이순신", 30)
+        binding.user = person
+
+    }
+
+    // 레이아웃에 onclick에 함수를 넣어주면 실행이 된다.
+    fun myClick(view: View) {
+        Log.d("MainActivity", "onclick")
+        testCount++
+
+        val person = Person("이순신", testCount)
+        binding.user = person
+
     }
 }
