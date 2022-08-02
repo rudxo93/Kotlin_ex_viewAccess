fed083ff88a61fcab0c7e2889955a75f7ed51293
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-02 11:18:12 +0900

    adapter생성 후 recyclerView에 연결하여 띄우기

--- ViewBindingAndAdapter/app/src/main/java/com/duran/viewbindingandadapter/MainActivity.kt
+++ ViewBindingAndAdapter/app/src/main/java/com/duran/viewbindingandadapter/MainActivity.kt
@@ -2,10 +2,47 @@ package com.duran.viewbindingandadapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+ViewBinding, DataBinding
+
+RecyclerView를 만들때 Adapter에서 findViewById 형식으로 아이템들을 찾아와서 view에 뿌려준다.
+-> 이러한 경우에도 viewBinding과 DataBinding을 이용해서 할 수 있나?
+*/
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val array = ArrayList<String>()
+
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("d")
+        array.add("e")
+        array.add("f")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("d")
+        array.add("e")
+        array.add("f")
+        array.add("a")
+        array.add("b")
+        array.add("c")
+        array.add("d")
+        array.add("e")
+        array.add("f")
+
+        val customAdapter = CustomAdapter(array)
+
+        val rv = findViewById<RecyclerView>(R.id.rv)
+        rv.adapter = customAdapter
+        rv.layoutManager = LinearLayoutManager(this)
     }
 }
