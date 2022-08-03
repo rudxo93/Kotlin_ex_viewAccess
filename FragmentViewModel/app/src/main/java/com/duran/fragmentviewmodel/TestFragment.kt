a58704d2ce1549ef7e1aee17067101fe8b7d61bc
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-03 12:38:56 +0900

    Fragment에서 dataBinding을 사용해서 간단한 계산기 예제 실행(화면 회전 시 값 초기화됨)

--- FragmentViewModel/app/src/main/java/com/duran/fragmentviewmodel/TestFragment.kt
+++ FragmentViewModel/app/src/main/java/com/duran/fragmentviewmodel/TestFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.duran.fragmentviewmodel.databinding.FragmentTestBinding
 
 /*
 공식문서 https://developer.android.com/guide/fragments/lifecycle 를 보게되면
@@ -14,6 +16,10 @@ Fragment에도 라이프사이클이 있다.
 */
 
 class TestFragment : Fragment() {
+
+    private lateinit var binding: FragmentTestBinding
+
+    var countValue = 0
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -31,7 +37,20 @@ class TestFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         Log.e("TestFragment", "onCreateView")
-        return inflater.inflate(R.layout.fragment_test, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_test, container, false)
+
+        binding.plus.setOnClickListener {
+            countValue++
+            binding.resultArea.text = countValue.toString()
+        }
+
+        binding.minus.setOnClickListener {
+            countValue--
+            binding.resultArea.text = countValue.toString()
+        }
+
+        return binding.root
     }
 
     override fun onResume() {
