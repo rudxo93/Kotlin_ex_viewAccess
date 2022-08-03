a51b94e4dc24828e16b71d70ad73864abc754aa8
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-03 15:17:34 +0900

    우선 Activity에서 viewModel을 사용하여 값이 저장되는지 test

--- ViewModelShare/app/src/main/java/com/duran/viewmodelshare/MainActivity.kt
+++ ViewModelShare/app/src/main/java/com/duran/viewmodelshare/MainActivity.kt
@@ -2,10 +2,37 @@ package com.duran.viewmodelshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.duran.viewmodelshare.databinding.ActivityMainBinding
+
+/*
+ViewModel -> Activity / Fragment 공유
+why?
+ -> activity의 값을 Fragment에서 쓰고 싶거나 Fragment에서 값을 ViewModel의 값으로
+    사용하기 위해서서*/
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.resultArea.text = viewModel.getCount().toString()
+
+        binding.plus.setOnClickListener {
+            viewModel.plus()
+            binding.resultArea.text = viewModel.getCount().toString()
+        }
+
+        binding.minus.setOnClickListener {
+            viewModel.minus()
+            binding.resultArea.text = viewModel.getCount().toString()
+        }
     }
 }
