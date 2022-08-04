d2a0019c0a2ebd21a885dff003d9dbae49a2637e
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-04 11:51:36 +0900

    LiveData와 ViewModel을 결합해서 간단하게 구현해보기

--- LiveDataWithViewModel/app/src/main/java/com/duran/livedatawithviewmodel/MainActivity.kt
+++ LiveDataWithViewModel/app/src/main/java/com/duran/livedatawithviewmodel/MainActivity.kt
@@ -2,10 +2,28 @@ package com.duran.livedatawithviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        findViewById<Button>(R.id.btnArea).setOnClickListener {
+            viewModel.plusLiveDataValue()
+        }
+
+        viewModel.testMutableLiveData.observe(this, Observer {
+            findViewById<TextView>(R.id.textArea).text = viewModel.testMutableLiveData.value!!.toString()
+        })
+
     }
 }
