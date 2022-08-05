987fa7e6f469e043d91504e402fe0992a9d3d90f
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-08-05 12:39:08 +0900

    fragment에서 viewLifecycleOwner사용과 viewModel 사용해보기

--- FragmentLiveDataAndViewLifecycleOwner/app/src/main/java/com/duran/fragmentlivedataandviewlifecycleowner/BlankFragment1.kt
+++ FragmentLiveDataAndViewLifecycleOwner/app/src/main/java/com/duran/fragmentlivedataandviewlifecycleowner/BlankFragment1.kt
@@ -6,10 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.duran.fragmentlivedataandviewlifecycleowner.databinding.FragmentBlank1Binding
 
 class BlankFragment1 : Fragment() {
 
+    // ViewBinding + LiveData
+
     private val TAG = "BlankFragment1"
+
+    private var _binding : FragmentBlank1Binding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: BlankViewModel1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +30,26 @@ class BlankFragment1 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank1, container, false)
+        _binding = FragmentBlank1Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+        viewModel = ViewModelProvider(this).get(BlankViewModel1::class.java)
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btn1.setOnClickListener {
+            viewModel.plusCountValue()
+        }
+
+        // viewLifecycleOwner에 대한 구글 공식문서
+        // https://developer.android.google.cn/codelabs/kotlin-android-training-live-data/index.html?index=..%2F..android-kotlin-fundamentals&hl=IT#4
+        viewModel.liveCount.observe(viewLifecycleOwner, Observer {
+            binding.text1.text = it.toString()
+        })
     }
 
     override fun onDestroyView() {
